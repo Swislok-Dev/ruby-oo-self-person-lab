806eb62bb2407f51e153488ced733e4d4b98c050
cmarker <cmarker4@gmail.com>
2020-11-06 09:36:51 -0600

    commit tree 8c3c250ebdb60311623d186f76e386c0d5783d9e

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,44 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+   def happiness
+    @happiness.clamp(0, 10)
+   end
+
+   def hygiene
+    @hygiene.clamp(0, 10)
+   end
+
+   def happy?
+    happiness > 7 ? true : false
+    end
+
+    def clean?
+        hygiene > 7 ? true : false
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        @hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+        
+    end
+
+    def work_out
+        @hygiene -= 3
+        hygiene
+    end
+
+end
